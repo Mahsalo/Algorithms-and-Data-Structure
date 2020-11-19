8860834edfee9433b14dfa5149e288b0eedd1e31
Mahsa Lotfi <mafilot@gmail.com>
2020-11-19 10:54:11 -0800

    Add files via upload

---
+++ CTCI_python/ch1_4_PalindromePermute.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 19 09:19:57 2020
+
+@author: mahsa
+"""
+class Solution:
+    def canPermutePalindrome(self, s: str) -> bool:
+        Ascii = [0]*256
+        cnt_odd=0
+        cnt_even=0
+        cnt_one=0
+        for i in range(len(s)):
+            Ascii[ord(s[i])]+=1
+        for j in range(len(Ascii)):
+            if Ascii[j]%2 == 1:
+                cnt_odd+=1
+            if Ascii[j]%2 == 0:
+                cnt_even+=1
+            if Ascii[j]==1:
+                cnt_one+=1    
+        if len(s)%2==0: 
+            if cnt_odd==0:
+                return True
+            else:
+                return False
+        else:
+            if cnt_odd>0 and cnt_one>1:
+                return False
+            if cnt_odd>0 and cnt_one<=1:
+                return True
+            
