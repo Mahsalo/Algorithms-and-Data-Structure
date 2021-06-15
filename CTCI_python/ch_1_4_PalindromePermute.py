23bec9fb394a11777880adb0fbac7f66496da2ce
Mahsa Lotfi <mafilot@gmail.com>
2021-06-15 11:26:49 -0700

    Update ch_1_4_PalindromePermute.py

--- CTCI_python/ch_1_4_PalindromePermute.py
+++ CTCI_python/ch_1_4_PalindromePermute.py
@@ -5,12 +5,20 @@ Created on Thu Nov 19 09:19:57 2020
 
 @author: mahsa
 """
+
 class Solution:
-    def canPermutePalindrome(self, s: str) -> bool:
+    
+    def Normalize(s):
+        return s.replace(" ","")
+    
+    
+    def canPermutePalindrome(self, s1: str) -> bool:
         Ascii = [0]*256
         cnt_odd=0
         cnt_even=0
         cnt_one=0
+        
+        s=Normalize(s1) ###remove the spaces
         for i in range(len(s)):
             Ascii[ord(s[i])]+=1
         for j in range(len(Ascii)):
@@ -28,6 +36,6 @@ class Solution:
         else:
             if cnt_odd>0 and cnt_one>1:
                 return False
-            if cnt_odd>0 and cnt_one<=1:
+            if cnt_odd==1:
                 return True
             
